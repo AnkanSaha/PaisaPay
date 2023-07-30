6e010b4f93fa59d187d59c75295e45ebd352bea9
Ankan Saha <ankansahadaluabari@gmail.com>
2023-07-30 21:21:02 +0530

    Added Data Models in MongoDB connections

---
+++ Server/source/settings/MongoDB/model/Client Account Data Model.ts
@@ -0,0 +1,6 @@
+export default {
+    ClientID: {type: Number, required: true},
+    Name: {type: String, required: true},
+    Email: {type: String, required: true},
+    Password: {type: String, required: true}
+}
