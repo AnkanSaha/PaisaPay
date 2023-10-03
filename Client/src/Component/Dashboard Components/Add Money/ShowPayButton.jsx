0f3b1468bc38cb376f80b39886fe33da39a2009b
Ankan Saha <ankansahadaluabari@gmail.com>
2023-10-04 00:25:54 +0530

    Added : Added  Stripe QR in Add Money Section

--- Client/src/Component/Dashboard Components/Add Money/ShowPayButton.jsx
+++ Client/src/Component/Dashboard Components/Add Money/ShowPayButton.jsx
@@ -1,37 +1,22 @@
 import React from "react"; // let's also import Component
-
-// Import Components
-import { Button } from "@chakra-ui/react"; // Chakra UI
-
-// Import Icons
-import { RiSecurePaymentFill } from "react-icons/ri"; // import the secure payment icon
+import StripeQR from "@public/icons/StripeQRPay.png"; // import our QR code
 
 export default function ShowPayButton() {
-    // States
-    const [isLoading, setIsLoading] = React.useState(false); // loading state
-  function ShowRazorPay() {
-    setIsLoading(true);
-    const Script = document.createElement("script");
-    const Form = document.createElement("form");
-    Script.src = "https://checkout.razorpay.com/v1/payment-button.js";
-    Script.setAttribute("data-payment_button_id", "pl_KhcCgUSXd5YyLN");
-    Form.appendChild(Script);
-    document.getElementById("PayButton").appendChild(Form);
-    setIsLoading(false);
-  }
-
   return (
     <>
-      <div id="PayButton" className="w-6/12 m-auto mt-16 space-y-6">
-        <Button
-          size="lg"
-          isLoading={isLoading}
-          leftIcon={<RiSecurePaymentFill />}
-          colorScheme="linkedin"
-          onClick={ShowRazorPay}
-        >
-          Payment Gateway
-        </Button>
+      <div className="">
+        <div id="PayButton" className="w-6/12 m-auto mt-16 space-y-6">
+          <stripe-buy-button
+            buy-button-id="buy_btn_1NxDMrSFtlKlFDXmqzfVPo5X"
+            publishable-key="pk_test_51MoPZpSFtlKlFDXms7pdG013rIK2admcMCyYcoRzDQaVWrPPrGDtb1NpRqNjY5z9lUzIlHeB3aD2iw2KzbpHtORG00NoS4lVYX"
+          ></stripe-buy-button>
+        </div>
+        <img
+          src={StripeQR}
+          width="190"
+          alt="StripeQR"
+          className="ml-5 mb-5 right-[25rem] absolute"
+        />
       </div>
     </>
   );
