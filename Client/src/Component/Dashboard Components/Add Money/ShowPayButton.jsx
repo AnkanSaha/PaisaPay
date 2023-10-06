f634df5fab5404c7c8319423637b41476efa1902
= <=>
2023-10-06 23:05:07 +0000

    Added : Added Payment Button

--- Client/src/Component/Dashboard Components/Add Money/ShowPayButton.jsx
+++ Client/src/Component/Dashboard Components/Add Money/ShowPayButton.jsx
@@ -1,23 +1,32 @@
-import React from "react"; // let's also import Component
-import StripeQR from "@public/icons/StripeQRPay.png"; // import our QR code
+import React from "react";
 
 export default function ShowPayButton() {
+  const LoadRazorPay = () => {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = "https://checkout.razorpay.com/v1/payment-button.js";
+      script.setAttribute(
+        "data-payment_button_id",
+        "pl_KhcCgUSXd5YyLN"
+      );
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.getElementById('PaisaADD').appendChild(script);
+      if(!document.getElementById('PaisaADD').hasChildNodes()){
+        document.getElementById('PaisaADD').appendChild(script);
+      }
+    });
+  };
+
+  React.useEffect(() => {
+    LoadRazorPay();
+  });
+
   return (
-    <>
-      <div className="">
-        <div id="PayButton" className="w-6/12 m-auto mt-16 space-y-6">
-          <stripe-buy-button
-            buy-button-id="buy_btn_1NxDMrSFtlKlFDXmqzfVPo5X"
-            publishable-key="pk_test_51MoPZpSFtlKlFDXms7pdG013rIK2admcMCyYcoRzDQaVWrPPrGDtb1NpRqNjY5z9lUzIlHeB3aD2iw2KzbpHtORG00NoS4lVYX"
-          ></stripe-buy-button>
-        </div>
-        <img
-          src={StripeQR}
-          width="190"
-          alt="StripeQR"
-          className="ml-5 mb-5 right-[25rem] absolute"
-        />
-      </div>
-    </>
+    <form id="PaisaADD" className="w-6/12 m-auto mt-16 space-y-6 space-x-3"></form>
   );
 }
