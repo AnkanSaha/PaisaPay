8546dcf974c31f63aabc671139d0758cf591078d
=Ankan Saha <=ankansahadaluabari@gmail.com>
2023-11-02 18:16:38 +0000

    Added: Added new REST Setup

--- Client/src/Helper/Auth/Authentication.jsx
+++ Client/src/Helper/Auth/Authentication.jsx
@@ -1,14 +1,11 @@
 // Import Storage
+import {API} from '@helper/Common'; // Import Common Functions
 
 // Function for Create Account
-export const Register = async (URL, Data) => {
+export const Register = async (Data) => {
   try {
-    const Result = await fetch(`${URL}/post/auth/create-new-account`, {
-      method: "POST",
-      body: Data,
-    }); // API Call for Create Account
-    const Response = await Result.json(); // Get Response
-    return Response; // Return Response
+    const Result = await API.Post(`/post/auth/create-new-account`, Data); // API Call for Create Account
+    return Result; // Return Response
   } catch (error) {
     console.log(error);
     return {
@@ -20,26 +17,7 @@ export const Register = async (URL, Data) => {
   }
 };
 
-// Function for Login
-export const Login = async (URL, Data) => {
-  try {
-    const Result = await fetch(`${URL}/post/auth/login-with-paisapay`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(Data),
-    }); // API Call for Login
-    const Response = await Result.json(); // Get Response
-    return Response; // Return Response
-  } catch (error) {
-    console.log(error);
-    return {
-      status: false,
-      statusCode: 507,
-      Title: "Error",
-      message: "Internal Server Error, Please try again later",
-    };
-  }
-};
+
 
 // Function for Forget Password
 export async function ForgetPasswordFinder(URL, Data) {
