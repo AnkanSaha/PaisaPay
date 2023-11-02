c008ebf550150168523b972a266786522eaca7aa
=Ankan Saha <=ankansahadaluabari@gmail.com>
2023-11-02 06:07:50 +0000

    Added : Added Get All HelpSupport History API PATH

--- Server/source/API/Get Request/Get Request Manager.ts
+++ Server/source/API/Get Request/Get Request Manager.ts
@@ -5,7 +5,8 @@ import {StringKeys} from '../../settings/keys/keys'; // Import StringKeys from k
 // Import All Sub Service Routes
 import AccountDetails from './Services/Account Details'; // Import Account Details
 import Authenticator from './Services/Authentication'; // Import Authentication
-
+import HelpCenter from './Services/Help Center'; // Import Help Center
+ 
 // Setup Config
 const Get_Request_Manager = Router(); // Setup Get_Request_Manager as Router
 Get_Request_Manager.use(CORS({origin: StringKeys.CORS_URL})); // Use CORS
@@ -13,6 +14,7 @@ Get_Request_Manager.use(CORS({origin: StringKeys.CORS_URL})); // Use CORS
 // All Sub Routes
 Get_Request_Manager.use('/AccountDetails', AccountDetails); // Use Account Details
 Get_Request_Manager.use('/Auth', Authenticator); // Use Account Details
+Get_Request_Manager.use('/help', HelpCenter); // Use Help Center
 
 // Export Get_Request_Manager
 export default Get_Request_Manager;
