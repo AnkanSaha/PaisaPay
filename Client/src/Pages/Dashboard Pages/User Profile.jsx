90fa95fb77c258f920064efc6af7738e93bced34
Ankan Saha <ankansahadaluabari@gmail.com>
2023-11-20 08:10:37 +0530

    Added : Added new Change Payment ID Option (partial)

--- Client/src/Pages/Dashboard Pages/User Profile.jsx
+++ Client/src/Pages/Dashboard Pages/User Profile.jsx
@@ -2,11 +2,11 @@ import React from 'react'; // import react
 
 // Import Components
 import Navbar from '@component/Navbar/Dashboard Navbar'; // import the navbar component
-import BalanceShow from '@component/Dashboard Components/overview/Balance Show'; // import the balance show component
 import ProfileDetails from '@component/Dashboard Components/User Profile/Profile Details'; // import the profile details component
 import Footer from '@component/Footer/General Footer'; // import the footer component
 import UpdateProfilePicture from '@component/Dashboard Components/User Profile/Update Profile Picture'; // import the update profile picture component
 import UpdateTransactionPIN from '@component/Dashboard Components/User Profile/Update Transaction PIN'; // import the update transaction pin component
+import UpdatePaymentID from '@component/Dashboard Components/User Profile/Update Payment ID';  // Import the Update Payment ID Components
 
 // import Functions
 import { Update_Document_Title } from '@helper/Common'; // import the function to update the document title
@@ -29,9 +29,9 @@ export default function UserProfile() {
 	return (
 		<>
 			<Navbar />
-			<BalanceShow />
 			<UpdateProfilePicture />
 			<UpdateTransactionPIN />
+			<UpdatePaymentID />
 			<ProfileDetails />
 			<Footer FooterStyle="fixed" />
 		</>
